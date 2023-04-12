c84441b3ed4c40e3d2543060d7aa1e9d514ae1fb
Luna R <imlvnaa@gmail.com>
2023-04-12 08:53:16 -0400

    a

---
+++ commands/test.js
@@ -0,0 +1,13 @@
+// access functions from index.js
+const root = require.main.exports;
+
+module.exports = {
+  name: 'test',
+  description: 'Test wether bot is receiving messages',
+  usage: '',
+  aliases: [],
+  execute(message, args) {
+
+    message.channel.send('Message Received');
+  }
+};
